023edfec1cea1e86ea7e42b068f892e5a82504f2
Jack Kilgore <kilgorej99@gmail.com>
2025-02-06 17:20:40 -0800

    chore: project naming

--- src/plugin/ProjectInfo.cpp
+++ src/plugin/ProjectInfo.cpp
@@ -4,5 +4,5 @@
 
 const int ProjectInfo::versionNumber = 0x000001;
 const char* const ProjectInfo::versionString = "0.0.1";
-const char* const ProjectInfo::companyName = "YOUR_COMPANY_NAME";
-const char* const ProjectInfo::projectName = "EXAMPLE";
+const char* const ProjectInfo::companyName = "Rejoice";
+const char* const ProjectInfo::projectName = "A Special Mystic Joy";
